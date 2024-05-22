83bc220310611db2fd87854ec8e2aa29e8ab41ae
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-05-22 13:35:17 +0200

    added new deploy

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,10 +4,6 @@ import { Helmet } from "react-helmet";
 import { GET_ALL_PORTFOLIOS } from "../graphql/queries";
 
 export default function HomePage() {
-	const { loading, error, data } = useQuery(GET_ALL_PORTFOLIOS);
-	if (loading) return <div>...Loading</div>;
-	if (error) return <div>...error</div>;
-	console.log(data);
 	return (
 		<div>
 			<Helmet>
