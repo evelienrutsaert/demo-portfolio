f51f339343a506e3e57080fc1eb0520187064653
evelienrutsaert <evelien.rutsaert@arteveldehs.be>
2024-05-22 13:23:11 +0200

    added new buid

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+	uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clw4n4vg80oj907w8xxycxvo6/master",
+	cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
